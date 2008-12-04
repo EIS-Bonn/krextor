551cb8e920be927296cc84b6eb398b307453ebdc
Christoph Lange <math.semantic.web@gmail.com>
2008-12-04 14:14:24 +0000

    Scala experiment
    git-svn-id: https://svn.kwarc.info/repos/swim/projects/krextor/trunk@578 ed9fab30-3827-0410-8f5f-a3ee7eb154d2

--- src/scala/src/info/kwarc/krextor/Krextor.scala
+++ src/scala/src/info/kwarc/krextor/Krextor.scala
@@ -7,24 +7,45 @@ object Krextor {
     printf("<%s> <%s> <%s> .\n", subjectURI, predicateURI, objectURI)
   }
 
-  def createResource(n : Node, t : Any) : Unit = {
-     outputTriple(n.label, "rdf:type", t.toString)
+  def createResource(n : Node, t : Any, baseURI : String) : Unit = {
+    
+    
+    val newBaseURI = baseURI + "/" + n.label + "-" + n.attribute("http://www.w3.org/XML/1998/namespace", "id").get
+    
+     outputTriple(newBaseURI, "rdf:type", t.toString)
+     
+     for (c <- n.attributes)
+       processAttribute(c.key, c.value.toString, newBaseURI)
+     
+     for (c <- n.child)
+       process(c, newBaseURI)
   }
   
-  def process(n : Node) : Unit = {
+  def addProperty(p : String, o : String, baseURI : String) : Unit = {
+     outputTriple(baseURI, p, o)
+  }
+  
+  def processAttribute(k : String, v : String, baseURI : String) : Unit = {
+    k match {
+      case "for" => addProperty("defines", v, baseURI)
+      case _ => ;
+    }
+  }
+  
+  def process(n : Node, baseURI : String) : Unit = {
     n match {
-      case t @ <omtext/> =>
-        createResource(n, if (!(t \ "@type").isEmpty) t \ "@type" else "other")
-      case _ =>
-        ;
+      case t @ <omtext>{ _* }</omtext> =>
+        createResource(n, t \ "@type", baseURI)
+      case _ => ;
     }
   }
   
   def main(args : Array[String]) : Unit = {
     val doc =
-      <omtext/>;
-      // <omtext type="definition"/>;
+      <omtext xml:id="foo" type="definition" for="#foo">
+        <omtext xml:id="bar" type="bla" for="#xyzzy"/>
+      </omtext>
     
-    process(doc);
+    process(doc, "");
   }
 }
